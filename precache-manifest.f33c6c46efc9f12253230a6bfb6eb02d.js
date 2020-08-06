51ab9c4eca623bd17637628ca3ae7238939199f1
femina123-design <62275491+femina123-design@users.noreply.github.com>
2020-08-06 08:59:59 +0530

    Updates

---
+++ precache-manifest.f33c6c46efc9f12253230a6bfb6eb02d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "570d2c086f17d9d7d42fdd20e3b92a8b",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "1314ba9218e1c85be797",
+    "url": "/todoApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d2d013e51fccceb726a",
+    "url": "/todoApp/static/js/2.66e42ca1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApp/static/js/2.66e42ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1314ba9218e1c85be797",
+    "url": "/todoApp/static/js/main.3bb514d9.chunk.js"
+  },
+  {
+    "revision": "9028c966e44e4edab0d2",
+    "url": "/todoApp/static/js/runtime-main.3c411f8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
